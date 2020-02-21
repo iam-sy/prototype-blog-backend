85cd636cc119a2c83d6ac3e0c1094cfb4a48cec9
moonseongyeon <xlrj0716@gmail.com>
2020-02-21 15:22:51 +0900

    first

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var path = require("path");
+var router = express.Router(); /* GET home page. */
+router.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
+
+module.exports = router;
