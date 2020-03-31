22ab4199507a7fa844ea81ab8cdd4b2ad7b280a6
sy.moon <xlrj0716@gmail.com>
2020-03-31 13:19:35 +0900

    test

--- imageServer.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import path from 'path';
-import logger from 'morgan';
-const app = express();
-app.use(logger('dev'));
-
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-module.exports = app;
